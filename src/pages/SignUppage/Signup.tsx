30cd5b46633733ff869537b1cc5ab4581113e021
gkdfo40 <gkdfo40@gmail.com>
2022-09-01 20:24:58 +0900

    refactor: refactoring SignUp component

--- src/pages/SignUppage/Signup.tsx
+++ src/pages/SignUppage/Signup.tsx
@@ -1,50 +1,75 @@
-import { isDisabled } from '@testing-library/user-event/dist/utils';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import './Signup.css';
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Signup.module.css";
 
 export default function Signup() {
-  const [inputEmail, setInputEmail] = useState('');
-  const [inputPassword, setInputPassword] = useState('');
-  const baseAPI = 'https://5co7shqbsf.execute-api.ap-northeast-2.amazonaws.com/production/';
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
+  const baseAPI =
+    "https://5co7shqbsf.execute-api.ap-northeast-2.amazonaws.com/production/";
   const navi = useNavigate();
 
   const validate = () => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-    return (regex.test(inputEmail) && inputPassword.length>=8);
-  }
+    return regex.test(inputEmail) && inputPassword.length >= 8;
+  };
 
   function BackToLogin() {
-    navi('/', { replace : true });
+    navi("/", { replace: true });
   }
 
   function SignUp() {
-    axios.post(baseAPI+`auth/signup`,
-      {
-        email : inputEmail,
-        password : inputPassword
-      },
-      {
-      headers : {
-        'Content-Type' : 'application/json'
+    axios
+      .post(
+        baseAPI + `auth/signup`,
+        {
+          email: inputEmail,
+          password: inputPassword,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-      }
-    ).then(() => navi('/'));
+      )
+      .then(() => navi("/"));
   }
 
-  return(
+  return (
     <div className="wrap">
-      <button className="backToLogin" onClick={() => { BackToLogin() }}>뒤로가기</button>
+      <button
+        className="backToLogin"
+        onClick={() => {
+          BackToLogin();
+        }}
+      >
+        뒤로가기
+      </button>
       <div className="signupMainFrame">
         <div className="idpw">
           <p>이메일</p>
-          <input type="email" placeholder="Email" onChange={(email) => setInputEmail(email.target.value)}></input>
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(email) => setInputEmail(email.target.value)}
+          ></input>
           <p>비밀번호</p>
-          <input type='password' placeholder="Password" onChange={(password) => setInputPassword(password.target.value)}></input>
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(password) => setInputPassword(password.target.value)}
+          ></input>
         </div>
-        <button type='button' className="signup" disabled={!validate()} onClick={SignUp}>회원가입</button>
+        <button
+          type="button"
+          className="signup"
+          disabled={!validate()}
+          onClick={SignUp}
+        >
+          회원가입
+        </button>
       </div>
-  </div>
-  )
+    </div>
+  );
 }
